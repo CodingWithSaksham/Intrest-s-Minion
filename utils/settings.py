3a3bd6d9a21fdc853c67c9fda57191067d9d2f3d
CodingWithFiery <sakshamthecoder@gmail.com>
2024-07-19 22:27:15 +0530

    Added functionality for voice commands

--- utils/settings.py
+++ utils/settings.py
@@ -9,9 +9,11 @@ load_dotenv()
 TOKEN = getenv("TOKEN")
 LOGGING_CHANNEL = int(getenv("LOGGING_CHANNEL"))
 WELCOME_CHANNEL_ID = int(getenv("WELCOME_CHANNEL"))
-BOT_CMD_ID = int(getenv("BOT_CMD_CHANNEL"))
 
 COGS_DIR = listdir('cogs')
+VOICE_COGS_DIR = listdir('voice_cogs')
+
+COGS = COGS_DIR + VOICE_COGS_DIR
 
 file = open('utils/loggers.json')
 logger_data = load(file)
